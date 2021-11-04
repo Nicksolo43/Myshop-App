c59d53d36d1cd46a9e42f19dde9a88e27c97580b
Akalingkaasi <nicksolo43@gmail.com>
2021-11-04 01:46:16 +0000

    start

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+   return(
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    );
+   
+};
+
+export default App;
