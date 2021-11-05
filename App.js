96e98d237aec1f9f6bf994c17d023d54813668a6
Akalingkaasi <nicksolo43@gmail.com>
2021-11-05 00:52:21 +0000

    First change

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import loginScreen from './screens/loginScreen';
+import homeScreen from './screens/homeScreen';
+import signupScreen from './screens/signupScreen';
+
+const Stack = createNativeStackNavigator();
 
 
-const App = () => {
-   return(
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
-    );
-   
-};
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options= {{headerShown: false}} name="Log in" component={loginScreen} />
+        <Stack.Screen options= {{headerShown: false}} name="Home" component={homeScreen} />
+        <Stack.Screen options= {{headerShown: false}} name="Sign up" component={signupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: { 
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
